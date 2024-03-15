959e0afd95258066350bca29935018339fe914dd
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-03-15 01:21:31 -0700

    Methods PUT and DELETE already created, and implemented

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -29,6 +29,32 @@ namespace Web_API_Authors.Controllers
             return Ok();
         }
 
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Author author, int id)
+        {
+            if(author.Id != id)
+            {
+                return BadRequest("Id dont match whith the author id");
+            }
+
+            context.Update(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete( int id) 
+        {
+            var exits = await context.Authors.AnyAsync(x => x.Id == id);
+            if (!exits)
+            {
+                return NotFound();
+            }
+            context.Remove(new Author { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
 
     }
 }
